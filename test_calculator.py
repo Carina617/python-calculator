3a086d13223a203cb8285a9c959191802f4595ae
Carina617 <jiahui.wu@student.mahidol.edu>
2024-11-24 11:51:34 +0000

    Completed unit test and bug fixes

--- test_calculator.py
+++ test_calculator.py
@@ -1,15 +1,29 @@
 import unittest
-from calculator import Calculator
+import calculator
+
 
 class TestCalculator(unittest.TestCase):
-
-    def setUp(self):
-        self.calc = Calculator()
-
     def test_add(self):
-        self.assertEqual(self.calc.add(1, 2), 3)
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-2, -3), -5)
+    
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(-5, -3), -2)
+    
+    def test_multiply(self):
+        self.assertEqual(multiply(4, 3), 12)
+        self.assertEqual(multiply(-4, -3), 12)
+    
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+        with self.assertRaises(ZeroDivisionError):
+            divide(10, 0)
+    
+    def test_modulo(self):
+        self.assertEqual(modulo(10, 3), 1)
+        with self.assertRaises(ZeroDivisionError):
+            modulo(10, 0)
 
-    # Add the following test methods to the TestCalculator class:
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
